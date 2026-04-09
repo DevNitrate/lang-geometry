6c0467eb1a861cff442b66818c289ee421384b76
DevNitrate <ambroiseinthewild@gmail.com>
2026-04-09 16:08:34 +0200

    text sanitization + bigram frequency counting

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,57 +1,74 @@
 use std::fs::read_to_string;
 
 use bevy::{ecs::component::Component, math::Vec2};
+use diacritics::remove_diacritics;
 
 #[derive(Component)]
-pub struct WordLength {
-    x_axis: Vec<f32>,
-    y_axis: Vec<f32>,
+pub struct BigramFreq {
+    bigrams: Vec<Vec<f32>>,
 
     // for drawing
     origin: Vec2,
-    y_scale: f32,
-    plot_length: f32,
-    pub vertices: Vec<Vec2>
+    plot_dimensions: Vec2,
 }
 
-impl WordLength {
-    pub fn new(corpus_path: &str, length: f32, y_scale: f32, origin: Vec2) -> Self {
+impl BigramFreq {
+    pub fn new(corpus_path: &str, origin: Vec2, plot_dimensions: Vec2) -> Self {
         let corpus_raw: String = read_to_string(corpus_path).unwrap();
-        let mut corpus_sanitized: String = String::new();
-
-        for c in corpus_raw.chars() {
-            match c {
-                '.' | ',' | '?' | '!' | ';' | ':' | '"' | '\n' | '\r' | '\t' => {},
-                _ => {
-                    corpus_sanitized.push(c);
-                }
-            }
-        }
+        let corpus_sanitized: String = sanitize_text(&corpus_raw);
         
-        let words: Vec<&str> = corpus_sanitized.split(" ").collect();
-        let y_axis: Vec<f32> = words.iter().map(|s| s.len() as f32).collect();
-
-        let mut x_axis: Vec<f32> = Vec::new();
-        let spacing: f32 = length / (y_axis.len() as f32);
-
-        for i in 0..y_axis.len() {
-            x_axis.push((i as f32) * spacing);
-        }
-
-        let mut vertices: Vec<Vec2> = Vec::new();
-
-        for i in 0..y_axis.len() {
-            vertices.push(Vec2::new(x_axis[i] + origin.x, (y_axis[i] * y_scale) + origin.y));
-        }
-
-        WordLength {
-            x_axis,
-            y_axis,
-
+        Self {
+            bigrams: get_bigrams(&corpus_sanitized),
             origin,
-            plot_length: length,
-            y_scale,
-            vertices
+            plot_dimensions
         }
     }
+}
+
+pub fn sanitize_text(txt: &str) -> String {
+    let txt: String = remove_diacritics(txt);
+    let mut text_sanitized: String = String::new();
+    let mut is_last_space: bool = true;
+
+    for c in txt.chars() {
+        if !(c.is_ascii_alphabetic() || (c == ' ' && !is_last_space)) {
+            continue;
+        }
+
+        is_last_space = c == ' ';
+
+        text_sanitized.push(c.to_ascii_lowercase());
+    }
+
+    text_sanitized.trim_ascii_end().to_string()
+}
+
+pub fn get_bigrams(txt: &str) -> Vec<Vec<f32>> {
+    let txt_chars: Vec<char> = txt.chars().collect();
+    let mut bigram_count: Vec<Vec<u32>> = vec![vec![0; 27]; 27];
+
+    if txt_chars.len() < 2 {
+        return vec![vec![0.0; 27]; 27];
+    }
+
+    for i in 0..(txt_chars.len() - 1) {
+        let curr: char = txt_chars[i];
+        let next: char = txt_chars[i+1];
+
+        let curr_idx: usize = if curr == ' ' {26} else {(curr as usize) - ('a' as usize)};
+        let next_idx: usize = if next == ' ' {26} else {(next as usize) - ('a' as usize)};
+
+        bigram_count[curr_idx][next_idx] += 1;
+    }
+
+    let total_bigrams: f32 = (txt_chars.len() - 1) as f32;
+    let mut bigram_freq: Vec<Vec<f32>> = vec![vec![0.0; 27]; 27];
+
+    for i in 0..27 {
+        for j in 0..27 {
+            bigram_freq[i][j] = (bigram_count[i][j] as f32) / total_bigrams;
+        }
+    }
+
+    bigram_freq
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::{DefaultPlugins, app::{App, PluginGroup, Startup, Update}, camera::{Camera2d, ClearColor}, color::Color, ecs::system::{Commands, Query}, gizmos::gizmos::Gizmos, math::Vec2, window::{MonitorSelection, Window, WindowMode, WindowPlugin}};
 
-use crate::corpus::WordLength;
+use crate::corpus::{BigramFreq};
 
 mod corpus;
 
@@ -15,18 +15,18 @@ fn main() {
         }))
         .insert_resource(ClearColor(Color::srgb_u8(0, 7, 20)))
         .add_systems(Startup, setup)
-        .add_systems(Update, draw_word_length)
+        // .add_systems(Update, draw_word_length)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 
-    commands.spawn(WordLength::new("corpus_en.txt", 1600.0, 10.0, Vec2::new(-800.0, -400.0)));
+    BigramFreq::new("test.txt", Vec2::new(-500.0, -500.0), Vec2::new(1000.0, 1000.0));
 }
 
-fn draw_word_length(mut gizmos: Gizmos, word_lengths: Query<&WordLength>) {
-    for word_length in word_lengths.iter() {
-        gizmos.linestrip_2d(word_length.vertices.clone(), Color::srgb_u8(181, 61, 61));
-    }
-}
+// fn draw_word_length(mut gizmos: Gizmos, word_lengths: Query<&WordLength>) {
+//     for word_length in word_lengths.iter() {
+//         gizmos.linestrip_2d(word_length.vertices.clone(), Color::srgb_u8(181, 61, 61));
+//     }
+// }
