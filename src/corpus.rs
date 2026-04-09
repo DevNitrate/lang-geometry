c38f664f4ec1ce26a6b57b796f16825e6a4777df
DevNitrate <ambroiseinthewild@gmail.com>
2026-04-10 00:08:28 +0200

    better plotting + text corpora for 10 languages

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,12 +1,13 @@
 use std::{f32::consts::PI, fs::read_to_string};
 
-use bevy::{asset::{Assets, Handle}, color::Color, ecs::{component::Component, system::{Commands, ResMut}}, math::{Quat, Vec2, primitives::Rectangle}, mesh::Mesh2d, prelude::Mesh, sprite::Text2d, sprite_render::{ColorMaterial, MeshMaterial2d}, text::TextFont, transform::components::Transform, ui::{Node, PositionType, widget::Text}};
+use bevy::{asset::{Assets, Handle}, color::Color, ecs::{component::Component, system::{Commands, ResMut}}, math::{Quat, Vec2, primitives::Rectangle}, mesh::Mesh2d, prelude::Mesh, sprite::Text2d, sprite_render::{ColorMaterial, MeshMaterial2d}, text::TextFont, transform::components::Transform};
 use diacritics::remove_diacritics;
 
 #[derive(Component)]
 pub struct BigramFreq {
     name: String,
     bigrams: Vec<Vec<f32>>,
+    max_freq: f32,
 
     // for drawing
     origin: Vec2,
@@ -17,16 +18,19 @@ impl BigramFreq {
     pub fn new(corpus_path: &str, name: &str, origin: Vec2, plot_dimensions: Vec2) -> Self {
         let corpus_raw: String = read_to_string(corpus_path).unwrap();
         let corpus_sanitized: String = sanitize_text(&corpus_raw);
+        let (bigrams, max_freq): (Vec<Vec<f32>>, f32) = get_bigrams(&corpus_sanitized);
         
         Self {
             name: name.to_string(),
-            bigrams: get_bigrams(&corpus_sanitized),
+            bigrams,
+            max_freq,
+            
             origin,
             plot_dimensions
         }
     }
 
-    pub fn spawn(&self, mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    pub fn spawn(&self, commands: &mut Commands, meshes: &mut ResMut<Assets<Mesh>>, materials: &mut ResMut<Assets<ColorMaterial>>) {
         let axis_thickness: f32 = 10.0;
         let rect_width: f32 = self.plot_dimensions.x / 27.0;
         let rect_height: f32 = self.plot_dimensions.y / 27.0;
@@ -47,13 +51,18 @@ impl BigramFreq {
             Transform::from_xyz(self.origin.x - (axis_thickness / 2.0) - 5.0, (self.plot_dimensions.y - axis_thickness * axis_knudge) / 2.0 + self.origin.y + axis_thickness, 0.0)
         ));
 
+        let rect: Handle<Mesh> = meshes.add(Rectangle::new(rect_width, rect_height));
+        let max_freq: f32 = self.max_freq;
+
         for i in 0..27 {
             for j in 0..27 {
-                let rect_material = materials.add(Color::srgb(self.bigrams[j][i] * 10.0, 0.0, 0.0));
-                let rect: Handle<Mesh> = meshes.add(Rectangle::new(rect_width, rect_height));
+                let freq: f32 = self.bigrams[j][i];
+                let t: f32 = freq / max_freq;
+                
+                let rect_material = materials.add(Color::srgba(0.949, 0.047, 0.317, t));
 
                 commands.spawn((
-                    Mesh2d(rect),
+                    Mesh2d(rect.clone()),
                     MeshMaterial2d(rect_material),
                     Transform::from_xyz(((j as f32)*rect_width + (rect_width / 2.0)) + self.origin.x, ((i as f32)*rect_height + (rect_height / 2.0)) + self.origin.y, 0.0)
                 ));
@@ -80,6 +89,15 @@ impl BigramFreq {
                 Transform::from_xyz(self.origin.x + self.plot_dimensions.x + (rect_width / 2.0), ((i as f32)*rect_height + (rect_height / 2.0)) + self.origin.y, 0.0).with_rotation(Quat::from_rotation_z(-PI / 2.0))
             ));
         }
+
+        commands.spawn((
+            Text2d::new(self.name.clone()),
+            TextFont {
+                font_size: rect_height,
+                ..Default::default()
+            },
+            Transform::from_xyz(self.origin.x - rect_height - 5.0, self.origin.y + (self.plot_dimensions.y) / 2.0, 0.0).with_rotation(Quat::from_rotation_z(PI / 2.0))
+        ));
     }
 }
 
@@ -101,12 +119,12 @@ pub fn sanitize_text(txt: &str) -> String {
     text_sanitized.trim_ascii_end().to_string()
 }
 
-pub fn get_bigrams(txt: &str) -> Vec<Vec<f32>> {
+pub fn get_bigrams(txt: &str) -> (Vec<Vec<f32>>, f32) {
     let txt_chars: Vec<char> = txt.chars().collect();
     let mut bigram_count: Vec<Vec<u32>> = vec![vec![0; 27]; 27];
 
     if txt_chars.len() < 2 {
-        return vec![vec![0.0; 27]; 27];
+        return (vec![vec![0.0; 27]; 27], 0.0);
     }
 
     for i in 0..(txt_chars.len() - 1) {
@@ -122,11 +140,17 @@ pub fn get_bigrams(txt: &str) -> Vec<Vec<f32>> {
     let total_bigrams: f32 = (txt_chars.len() - 1) as f32;
     let mut bigram_freq: Vec<Vec<f32>> = vec![vec![0.0; 27]; 27];
 
+    let mut max_freq: f32 = 0.0;
+
     for i in 0..27 {
         for j in 0..27 {
-            bigram_freq[i][j] = (bigram_count[i][j] as f32) / total_bigrams;
+            let freq: f32 = (bigram_count[i][j] as f32) / total_bigrams;
+
+            max_freq = max_freq.max(freq);
+            
+            bigram_freq[i][j] = freq;
         }
     }
 
-    bigram_freq
+    (bigram_freq, max_freq)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{DefaultPlugins, app::{App, PluginGroup, Startup, Update}, asset::Assets, camera::{Camera2d, ClearColor}, color::Color, ecs::system::{Commands, Query, ResMut}, gizmos::gizmos::Gizmos, math::Vec2, sprite_render::ColorMaterial, window::{MonitorSelection, Window, WindowMode, WindowPlugin}, prelude::Mesh};
+use bevy::{DefaultPlugins, app::{App, PluginGroup, Startup}, asset::Assets, camera::{Camera2d, ClearColor, OrthographicProjection, Projection}, color::Color, ecs::{system::{Commands, ResMut}}, math::Vec2, prelude::Mesh, sprite_render::ColorMaterial, transform::components::Transform, window::{MonitorSelection, Window, WindowMode, WindowPlugin}};
 
 use crate::corpus::{BigramFreq};
 
@@ -15,20 +15,43 @@ fn main() {
         }))
         .insert_resource(ClearColor(Color::srgb_u8(0, 7, 20)))
         .add_systems(Startup, setup)
-        // .add_systems(Update, draw_word_length)
+        // .add_systems(Update, move_around)
         .run();
 }
 
-fn setup(mut commands: Commands, meshes: ResMut<Assets<Mesh>>, materials: ResMut<Assets<ColorMaterial>>) {
-    commands.spawn(Camera2d);
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        Projection::from(OrthographicProjection {
+            scale: 1.0,
+            ..OrthographicProjection::default_2d()
+        })
+    ));
 
-    let big: BigramFreq = BigramFreq::new("corpus_en.txt", "english", Vec2::new(-500.0, -500.0), Vec2::new(1000.0, 1000.0));
+    let lang: &str = "tagalog";
+    let french_corpus: BigramFreq = BigramFreq::new(format!("corpora/{}.txt", lang).as_str(), lang, Vec2::new(-825.0, -400.0), Vec2::new(750.0, 750.0));
+    let english_corpus: BigramFreq = BigramFreq::new("corpora/english.txt", "english", Vec2::new(100.0, -400.0), Vec2::new(750.0, 750.0));
 
-    big.spawn(commands, meshes, materials);
+    french_corpus.spawn(&mut commands, &mut meshes, &mut materials);
+    english_corpus.spawn(&mut commands, &mut meshes, &mut materials);
 }
 
-// fn draw_word_length(mut gizmos: Gizmos, word_lengths: Query<&WordLength>) {
-//     for word_length in word_lengths.iter() {
-//         gizmos.linestrip_2d(word_length.vertices.clone(), Color::srgb_u8(181, 61, 61));
+// fn move_around(mut evr_motion: MessageReader<MouseMotion>, buttons: Res<ButtonInput<MouseButton>>, mut cameras: Query<(&mut Transform, &mut Projection), With<Camera2d>>, mw: Res<AccumulatedMouseScroll>) {
+//     for (mut transform, proj) in cameras.iter_mut() {
+//         if buttons.pressed(MouseButton::Left) {
+//             for ev in evr_motion.read() {
+//                 transform.translation.x -= ev.delta.x;
+//                 transform.translation.y += ev.delta.y;
+//             }
+//         }
+//
+//         match *proj.into_inner() {
+//             Projection::Orthographic(ref mut orth) => {
+//                 let zoom = -mw.delta.y * 0.1;
+//                 orth.scale *= 1.0 + zoom;
+//             },
+//             _ => unreachable!()
+//         }
 //     }
 // }
